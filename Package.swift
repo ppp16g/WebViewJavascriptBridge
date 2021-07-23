6010b82d1032935e65c7370f3db6b3e8e150d3e2
fukuda-ku <fukuda-kuk@mb.njk.co.jp>
2021-07-23 15:07:48 +0900

    add_spm

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WebViewJavascriptBridge",
+    products: [
+        .library(
+            name: "WebViewJavascriptBridge",
+            targets: ["WebViewJavascriptBridge"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "WebViewJavascriptBridge",
+            path: "WebViewJavascriptBridge")
+    ]
+)
